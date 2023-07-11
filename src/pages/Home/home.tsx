ec00bde70770f560c69e5d8451ae5d7ba6b80e3f
vitamin777 <rlaalsdn8@gmail.com>
2023-07-11 22:27:53 +0900

    feat(router setting):포맷터 prettier 설치, react-router-dom 설치, 메인 홈 페이지 라우터 경로 추가, gitignore(node,windows) 추가
    - 포맷팅을 위해 prettier 설치 및 사용
    -01- 경로 설정을 위해 react-router-dom 설치 후 src/page/home/Home.tsx 파일 생성
    -02- src/util 폴더 생성 후 router.tsx 파일 생성하여 element 에 Home 컴포넌트 설정 및 path 경로 설정
    -03- App.tsx 에 react-router-dom 에서 제공하는 createBrowserRouter, RouterProvider 를 활용해 RouterObject 에 RouterInfo(router.tsx 에서 설정한 export const RouterInfo(router 설정)) 라우터 객체 대입해 RouterProvider 의 속성 router 에 설정(router 6.4 version 부터 이런식으로 활용한다고 하길래 따라해봄...)
    - App.tsx 에 Suspense tag 는 후에 로딩 스피너 이미지를 넣을 생각 현재는 하드코딩으로 대체함

---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,5 @@
+export const Home = () => {
+    return (
+        <div>vitamin777 Main Home</div>
+    )
+}
---
+++ src/util/router.tsx
@@ -0,0 +1,9 @@
+import {Home} from "../pages/Home/home";
+
+export const RouterInfo = [
+    {
+        path: "/",
+        element: <Home />,
+        label: "home"
+    }
+]
